41bae01008ee71393cb53bfe70a85b4b3831db4e
VitaliiRublevskii <148254914+VitaliiRublevskii@users.noreply.github.com>
2024-07-05 18:21:57 +0300

    Add search VIN

--- VVinCar/src/components/App/App.tsx
+++ VVinCar/src/components/App/App.tsx
@@ -1,18 +1,21 @@
+
 import styled from "styled-components";
 import 'bootstrap/dist/css/bootstrap.min.css'; // Убедитесь, что Bootstrap CSS импортирован
+import VinAuto from './VinAuto'; // Предполагаем, что VinAuto - это ваш компонент для поиска VIN
 
 const StyledApp = styled.div`
-  padding: 20px; // Добавьте некоторые стили, если нужно
+  padding: 20px; // Немного паддинга для красоты
 `;
 
 const App = () => {
     return (
       <StyledApp>
         <div className="container">
-          <h1 className="text-center mt-5">Привет, Bootstrap!</h1>
-          <p className="text-muted text-center">Это пример использования Bootstrap в вашем React-приложении.</p>
+          <h1 className="text-center mt-5">Поиск истории авто по VIN номеру</h1>
+          <VinAuto /> 
+          <p className="text-muted text-center">Используйте форму выше для поиска по VIN.</p>
           <div className="d-flex justify-content-center">
-            <button className="btn btn-primary">Нажми меня</button>
+            <button className="btn btn-primary">Нажми меня</button> 
           </div>
         </div>
       </StyledApp>
